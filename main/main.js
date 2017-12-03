05076168320dc825b6011f512809a0dae8741a22
fengyu_u <395861552@qq.com>
2017-12-03 12:28:05 +0800

    1203

--- main/main.js
+++ main/main.js
@@ -1,6 +1,23 @@
 // Write your cade below:
-function main() {};
+function main(real,input) {
+    let a = 0;
+    let b = 0;
+    console.log(a);
+    input = input.split('');
+    real = real.split('');
 
-module.exports = {
-    main
+    input.map((temp) =>{
+        let comp = real.map((tt,ind,array) =>{
+            if(array.indexOf(temp)>-1){
+                a +=1;
+            }
+
+        });
+        console.log(a);
+    });
+
+
 }
+
+
+module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,10 @@
 const main = require('../main/main');
-
+let expect = require('chai').expect;
 describe('main()', () => {
-
+    it('real:1234,input:1234',function () {
+       let real = '1234';
+       let input = '1234';
+       let result = main(real,input);
+       expect(result).to.equal([1234]);
+    });
 });
